875d4228063125b484f47c02fdc4d9e966845c61
tomcatling <tomcatling@gmail.com>
2018-03-04 16:50:25 +0000

    fixed column script

--- fix_columns.sh
+++ fix_columns.sh
@@ -8,13 +8,13 @@ do
 	tune_start=$(cat $tune | grep -n -m 1 '|' | sed 's/\([0-9]*\).*/\1/')
 
 	# get the header
-	header=$(head -n $(echo "$tune_start-2" | bc) $tune)
+	header=$(head -n $(echo "$tune_start-1" | bc) $tune)
 
 	# echo the header into a temporary file
-	echo "$header" | tail -n +2 > "$tune.cols"
+	echo "$header" | tail -n +1 > "$tune.cols"
 
 	# get the tune body
-	tune_body=$(tail -n +$(echo "$tune_start-1" | bc) $tune)
+	tune_body=$(tail -n +"$tune_start" $tune)
 
 	# process the body
 	# 	first replace any | with @|
